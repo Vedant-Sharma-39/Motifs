e570d6b0890a6976abee9107dc23fe229ee3662d
Vedant-Sharma-39 <74167738+Vedant-Sharma-39@users.noreply.github.com>
2022-09-22 02:23:11 +0530

    Updated Motif.py
    Improved Visuals and Code.

--- motif.py
+++ motif.py
@@ -6,17 +6,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-st.write('Select the thresholds for Y -> Z and X -> Z')
+st.markdown(r"""## Feed-Forward loop
+Select thresholds for $ Y  \rightarrow Z $ and $ X \rightarrow Z $""")
 
-K_YZ = st.slider('K_YZ', min_value=0.1,
-                 max_value=2.0, value=1.0)
+col_1, col_2 = st.columns(2)
+
+with col_1:
+
+    st.header(r'$K_{YZ}$')
+    st.markdown(r'Observe the delay effect on "ON" depends on $K_{YZ}$')
+    K_YZ = st.slider('', min_value=0.1,
+                     max_value=2.0, value=1.0)
+
+with col_2:
+    st.header(r'$K_{XZ}$')
+    st.markdown(r'What happens if you increase way too much')
+    K_XZ = st.slider('', min_value=0.1,
+                     max_value=1.2, value=0.5)
 
 
-def FFL_Model(t, RNA, K_YZ):
+def FFL_Model(t, RNA, K_YZ, K_XZ):
 
     K_XY = 0.5
-
-    K_XZ = 0.5
     a_Y = 0.2
     a_Z = 0.2
     b_Y = 1
@@ -31,22 +42,37 @@ def FFL_Model(t, RNA, K_YZ):
     ]
 
 
-t = np.linspace(0, 5, 100)
+t = np.linspace(0, 14, 100)
 
 sol = solve_ivp(FFL_Model, t_span=[0, 10], y0=(
-    1, 0, 0), args=(K_YZ,), dense_output=True)
+    1, 0, 0), args=(K_YZ, K_XZ,), dense_output=True)
 
 rise = sol.sol(t)
 solutions = rise.T[-1]
 
 _, y, z = solutions
 sol = solve_ivp(FFL_Model, t_span=[0, 10], y0=(
-    0, y, z), args=(K_YZ,), dense_output=True)
+    0, y, z), args=(K_YZ, K_XZ,), dense_output=True)
 fall = sol.sol(t)
 
-sns.set_style('darkgrid')
 
 lines = np.vstack((rise.T, fall.T))
-
 data = pd.DataFrame(lines, columns=['X', 'Y', 'Z'])
-st.line_chart(data)
+data['time'] = np.linspace(0, 28, 200)
+data = data.melt(value_vars=['X', 'Y', 'Z'], id_vars=['time'])
+
+t_star = data[(data['variable'] == 'Y') & (
+    data['value'] > K_YZ)].iloc[0]['time']
+
+line = alt.Chart(pd.DataFrame({'y': [t_star]})).mark_rule(color='orange', strokeDash=(5, 5)).encode(
+    x='y')
+
+Chart = alt.Chart(data).mark_line().encode(
+    alt.X('time', title='Time'),
+    alt.Y('value', title='Value'),
+    color='variable',
+).properties(
+    width=700
+)
+
+st.altair_chart(line + Chart)
