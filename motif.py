56472caec07e24a7ac3b1cf4d8b11a17e07494b4
Vedant Sharma <74167738+Vedant-Sharma-39@users.noreply.github.com>
2023-04-26 02:28:17 +0530

    Update motif.py

--- motif.py
+++ motif.py
@@ -1,78 +1,94 @@
-import streamlit as st
-import altair as alt
 import numpy as np
-from scipy.integrate import solve_ivp
+from collections import Counter
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
+import math
+sns.set_style('darkgrid')
 
-st.markdown(r"""## Feed-Forward loop
-Select thresholds for $ Y  \rightarrow Z $ and $ X \rightarrow Z $""")
+import numpy as np
+from collections import Counter
+import math
 
-col_1, col_2 = st.columns(2)
-
-with col_1:
-
-    st.header(r'$K_{YZ}$')
-    st.markdown(r'Observe the delay effect on "ON" depends on $K_{YZ}$')
-    K_YZ = st.slider('', min_value=0.1,
-                     max_value=2.0, value=1.0)
-
-with col_2:
-    st.header(r'$K_{XZ}$')
-    st.markdown(r'What happens if you increase way too much')
-    K_XZ = st.slider('', min_value=0.1,
-                     max_value=1.2, value=0.5)
-
-
-def FFL_Model(t, RNA, K_YZ, K_XZ):
-
-    K_XY = 0.5
-    a_Y = 0.2
-    a_Z = 0.2
-    b_Y = 1
-    b_Z = 1
-
-    X, Y, Z = RNA
-
-    return [
-        0,
-        b_Y * int(X > K_XY) - a_Y * Y,
-        b_Z * int(X > K_XZ) * int(Y > K_YZ) - a_Z * Z
-    ]
+def update(r_prey, d_prey, e_pred, d_pred):
+    def wrap(prey, predator):
+    
+        
+        updated_prey =int(r_prey* prey - d_prey * predator)
+        updated_predator = int(e_pred * d_prey - predator * d_pred)
+        
+        new_prey = max(updated_prey, 0)
+        new_predator = max(updated_predator, 0)
+        
+        return (new_prey, new_predator)
+    return wrap
 
 
-t = np.linspace(0, 14, 100)
 
-sol = solve_ivp(FFL_Model, t_span=[0, 10], y0=(
-    1, 0, 0), args=(K_YZ, K_XZ,), dense_output=True)
+class SiteModel:
 
-rise = sol.sol(t)
-solutions = rise.T[-1]
+    def __init__(self, n_sites, initial_pop, update_pop):
+        self.n_sites = n_sites
+        self.prey_pop = [initial_pop[0]]
+        self.pred_pop = [initial_pop[1]]
+        self.update_pop = update_pop
+        self.pop_history_prey = []
+        self.pop_history_pred = []
 
-_, y, z = solutions
-sol = solve_ivp(FFL_Model, t_span=[0, 10], y0=(
-    0, y, z), args=(K_YZ, K_XZ,), dense_output=True)
-fall = sol.sol(t)
+    def distribute_populations(self):
+        probabilities = np.ones(self.n_sites) / self.n_sites
+        allotments_prey = np.random.choice(np.arange(1, self.n_sites + 1), size=self.prey_pop[-1], p=probabilities)
+        allotments_pred = np.random.choice(np.arange(1, self.n_sites + 1), size=self.pred_pop[-1], p=probabilities)
 
+        counts_prey = Counter(allotments_prey)
+        counts_pred = Counter(allotments_pred)
 
-lines = np.vstack((rise.T, fall.T))
-data = pd.DataFrame(lines, columns=['X', 'Y', 'Z'])
-data['time'] = np.linspace(0, 28, 200)
-data = data.melt(value_vars=['X', 'Y', 'Z'], id_vars=['time'])
+        return counts_prey, counts_pred
 
-t_star = data[(data['variable'] == 'Y') & (
-    data['value'] > K_YZ)].iloc[0]['time']
+    def iterate(self, num_iterations):
+        for _ in range(num_iterations):
+            counts_prey, counts_pred = self.distribute_populations()
 
-line = alt.Chart(pd.DataFrame({'y': [t_star]})).mark_rule(color='orange', strokeDash=(5, 5)).encode(
-    x='y')
+            new_prey_pop = 0
+            new_pred_pop = 0
+            for site in range(1, self.n_sites + 1):
+                prey_site = counts_prey.get(site, 0)
+                pred_site = counts_pred.get(site, 0)
 
-Chart = alt.Chart(data).mark_line().encode(
-    alt.X('time', title='Time'),
-    alt.Y('value', title='Value'),
-    color='variable',
-).properties(
-    width=700
-)
+                updated_prey, updated_pred = self.update_pop(prey_site, pred_site)
+                new_prey_pop += updated_prey
+                new_pred_pop += updated_pred
 
-st.altair_chart(line + Chart)
+            self.prey_pop.append(new_prey_pop)
+            self.pred_pop.append(new_pred_pop)
+
+            populations_prey = np.zeros(self.n_sites)
+            for site, count in counts_prey.items():
+                populations_prey[site-1] = count
+            self.pop_history_prey.append(populations_prey)
+
+            populations_pred = np.zeros(self.n_sites)
+            for site, count in counts_pred.items():
+                populations_pred[site-1] = count
+            self.pop_history_pred.append(populations_pred)
+            
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Define the model parameters and run the simulation
+k, r_prey, e_pred, d_prey = 10, 2, 0.5, 0.5
+mod = SiteModel(n_sites=10, initial_pop=(30, 5), update_pop=update(k, r_prey, e_pred, d_prey))
+mod.iterate(100)
+
+# Create the plot
+fig, ax = plt.subplots()
+ax.plot(mod.prey_pop, label='Prey Population')
+ax.plot(mod.pred_pop, label='Predator Population')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Population')
+ax.set_title(f'r_prey: {r_prey:.1f}, d_prey: {d_prey:.1f}')
+ax.legend()
+plt.tight_layout()
+
+# Display the plot on Streamlit
+st.pyplot(fig)
+         
